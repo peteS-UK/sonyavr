563ef8719f6ad2cad37dea32325559b09c65dc0d
peteS-UK <peter@sketch.cc>
2024-12-29 11:16:28 +0000

    Fix for optionsflow config_entry deprecation

--- custom_components/sonyavr/config_flow.py
+++ custom_components/sonyavr/config_flow.py
@@ -76,13 +76,13 @@ class SonyAVRConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @callback
     def async_get_options_flow(config_entry):
         """Get the options flow for this handler."""
-        return OptionsFlowHandler(config_entry)
+        return OptionsFlowHandler()
 
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+    def __init__(self) -> None:
         """Initialize options flow."""
-        self.config_entry = config_entry
+        # self.config_entry = config_entry
 
     async def async_step_init(self, user_input: dict[str, Any] | None = None):
         """Manage the options."""
