ed48c885cd6cef767efac00bdc41b609ff0ffebc
peteS-UK <64092177+peteS-UK@users.noreply.github.com>
2026-03-10 16:30:48 +0000

    correction

--- custom_components/sonyavr/media_player.py
+++ custom_components/sonyavr/media_player.py
@@ -66,7 +66,7 @@ async def async_setup_entry(
 
     sonyavr = config["sonyavr"]
 
-    async_add_entities([SonyAVRDevice(sonyavr, hass)])
+    async_add_entities([SonyAVRDevice(sonyavr, config_entry)])
 
     # Register entity services
     platform = entity_platform.async_get_current_platform()
@@ -85,9 +85,9 @@ async def async_setup_entry(
 class SonyAVRDevice(MediaPlayerEntity):
     # Representation of a Sony AVR
 
-    def __init__(self, device, hass):
+    def __init__(self, device, config_entry):
         self._device = device
-        self._hass = hass
+        self._config_entry = config_entry
         self._entity_id = "media_player.sonyavr"
         self._unique_id = "sonyavr_" + self._device.name.replace(" ", "_").replace(
             "-", "_"
@@ -96,10 +96,10 @@ class SonyAVRDevice(MediaPlayerEntity):
         self._notifier_task: asyncio.Task | None = None
 
     async def _async_startup(self, loop):
-        self._notifier_task = self._hass.async_create_background_task(
+        self._notifier_task = self.hass.async_create_background_task(
             self._device.run_notifier(), name="sonyavr notifier task"
         )
-        self._ping_task = self._hass.async_create_background_task(
+        self._ping_task = self.hass.async_create_background_task(
             self._device.run_ping_watcher(), name="sonyavr ping watcher task"
         )
         await self._device.command_service.async_connect()
@@ -109,7 +109,7 @@ class SonyAVRDevice(MediaPlayerEntity):
         if self._device.state_service.power is None:
             _LOGGER.debug("Power state is uninitialised, so initialising all states")
             _power_state = await self._device.async_get_power_state()
-            _power_cycle = self.config_entry.options.get(CONF_POWER_CYCLE_INIT, True)
+            _power_cycle = self._config_entry.options.get(CONF_POWER_CYCLE_INIT, True)
 
             # Turn on and off to force the feedback
             if not _power_state and _power_cycle:
@@ -126,7 +126,7 @@ class SonyAVRDevice(MediaPlayerEntity):
         """Subscribe to device events."""
         self._device.set_update_cb(self.async_update_callback)
 
-        async_at_started(self._hass, self._async_startup)
+        async_at_started(self.hass, self._async_startup)
 
     def async_update_callback(self, reason=False):
         """Update the device's state."""
